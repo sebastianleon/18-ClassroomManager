28c64c7bb605975f452c575ae2d7e4b1d3e34439
Juan Leon <Juan Leon>
2016-09-12 09:40:36 -0700

    first commit

---
+++ server/Classroom/Classroom.API/App_Start/WebApiConfig.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace Classroom.API
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+
+            // Enable COrs
+            var policy = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(policy);
+            //Turn on Camel Case contract resolver
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver =
+                new CamelCasePropertyNamesContractResolver();
+
+            // ignore circular refrence errors
+            config.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling =
+               ReferenceLoopHandling.Ignore;
+
+            var appXmlType = config.Formatters.XmlFormatter.SupportedMediaTypes.FirstOrDefault(t => t.MediaType == "application/xml");
+            config.Formatters.XmlFormatter.SupportedMediaTypes.Remove(appXmlType);
+
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ server/Classroom/Classroom.API/Infrastructure/ClassroomDataContext.cs
@@ -0,0 +1,45 @@
+﻿using Classroom.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Classroom.API.Infrastructure
+{
+    public class ClassroomDataContext : DbContext
+    {
+        public ClassroomDataContext() : base("ClassroomManager")
+        {
+
+        }
+
+        public IDbSet<Assignment> Assignments { get; set; }
+        public IDbSet<Project> Projects { get; set; }
+        public IDbSet<Student> Students { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+
+            //Compound Key
+            modelBuilder.Entity<Assignment>()
+                        .HasKey(a => new { a.StudentId, a.ProjectId }); 
+
+
+            //A project has many assignments
+            modelBuilder.Entity<Project>()
+                        .HasMany(p => p.Assignments)
+                        .WithRequired(a => a.Project)
+                        .HasForeignKey(a => a.ProjectId);
+
+
+            //A student has many assignments
+            modelBuilder.Entity<Student>()
+                        .HasMany(s => s.Assignments)
+                        .WithRequired(a => a.Student)
+                        .HasForeignKey(a => a.StudentId);
+
+        }
+
+    }
+}
